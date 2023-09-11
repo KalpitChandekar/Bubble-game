d63539c8e88d42c277a579734cecd59f08a610b9
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-11 14:11:37 +0530

    render diff no in diff bubbles by random function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var clutter = "";
 
-for (var i = 0; i <= 239; i++) {
-  clutter += `<div class="bubble">5</div>`;
+for (var i = 1; i <= 240; i++) {
+  var rn = Math.floor(Math.random() * 10);
+  clutter += `<div class="bubble">${rn}</div>`;
 }
 
 document.getElementById("pbtm").innerHTML = clutter;
